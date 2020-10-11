41443f5eb00ed370465a0aa48b675b26314b9bb2
Yuri <iurii@chizhov.in>
2020-10-11 08:44:04 +0300

    Допилил тебе поиск по совпадениям в 2 списках

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ CHAT_LIST = [-1001062351210, -1001403651762, -1001221148335] + [-1001425875581, 
                                                                 -1001274730605, -1001404652265,
                                                                 -1001244025771, -1001405491023, -1001392392805]
 KEY_WORDS = {"test", }
+KEY_WORDS_2 = {"test2", }
 
 client = TelegramClient(username, api_id, api_hash)
 
@@ -32,15 +33,15 @@ def to_lower(word: str):
 
 @client.on(events.NewMessage(chats=CHAT_LIST))
 async def message_parser(event: events.newmessage.NewMessage.Event):
-    global KEY_WORDS
+    global KEY_WORDS, KEY_WORDS_2
     base_len = len(KEY_WORDS)
+    base_len_2 = len(KEY_WORDS_2)
     message_text_set = event.raw_text.split(" ")
     message_text_set = set(map(to_lower, message_text_set))
     new_len = len(KEY_WORDS - message_text_set)
-
-    if new_len < base_len:
+    new_len_2 = len(KEY_WORDS_2 - message_text_set)
+    if (new_len < base_len) and (new_len_2 < base_len_2):
         channel_id_ = event.chat_id
-
         group_: Channel = await client.get_entity(channel_id_)
         uname_ = group_.username
         if username:
@@ -48,10 +49,9 @@ async def message_parser(event: events.newmessage.NewMessage.Event):
         else:
             link_ = "У этой группы нет ссылки для доступа."
         await event.forward_to(USER_TO_SEND)
-        await client.send_message(USER_TO_SEND, f"{KEY_WORDS & message_text_set}"
+        await client.send_message(USER_TO_SEND, f"{KEY_WORDS_2 & message_text_set} and {KEY_WORDS & message_text_set}"
                                                 f"\n"
                                                 f"Группа: {link_}")
-        # await client.send_message(USER_TO_SEND, event.chat)
 
 
 with client:
